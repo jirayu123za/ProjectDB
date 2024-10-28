d4d6ddd2ace710a2622e2c90f3567031e05c40b9
jirayu <jirayu004za@gmail.com>
2024-10-29 00:11:39 +0700

    update api with register & login

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         $payload = $request->validate([
             "first_name" => "required|min:2|max:25",
@@ -23,10 +24,58 @@ class AuthController extends Controller
         try {
             $payload["password"] = Hash::make($payload["password"]);
             User::create($payload);
-            return response()->json(["status" => 200, "message" => "Account created successfully!"]);
+            return response()->json([
+                "status" => 200,
+                "message" => "Account created successfully!"
+            ]);
         } catch (\Exception $err) {
             Log::info("user_register_err =>" . $err->getMessage());
-            return response()->json(["status" => 500, "message" => "Something went wrong!"], 500);
+            return response()->json([
+                "status" => 500,
+                "message" => "Something went wrong!"],
+                500
+            );
         }
     }
+
+    public function login(Request $request)
+    {
+        $payload = $request->validate([
+            // "email" => "required|email",
+            "user_name" => "required|alpha_num:ascii",
+            "password" => "required"
+        ]);
+
+        try {
+            // $user = User::where("email", $payload["email"])->first();
+            $user = User::where("user_name", $payload["user_name"])->first();
+            if ($user) {
+                if (!Hash::check($payload["password"], hashedValue: $user->password)) {
+                    return response()->json([
+                        "status" => 401,
+                        "message" => "Invalid credentials."
+                    ]);
+                }
+
+                $token = $user->createToken("web")->plainTextToken;
+                $authRes = array_merge($user->toArray(), ["token" => $token]);
+                return ["status" => 200,
+                        "user" => $authRes,
+                        "message" => "Loggedin succssfully!"
+                    ];
+            }
+            return response()->json([
+                "status" => 401,
+                "message" => "No account found with these credentials."
+            ]);
+        } catch (\Exception $err) {
+            Log::info("user_register_err =>" . $err->getMessage());
+            return response()->json([
+                "status" => 500,
+                "message" => "Something went wrong!"],
+                500
+            );
+        }
+    }
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post("/auth/register", [AuthController::class, 'register']);
+Route::post("/auth/login", [AuthController::class, 'login']);
