e439efc853e46dc52cd6f9ea392dc88d17f79492
jirayu <jirayu004za@gmail.com>
2024-10-30 03:02:41 +0700

    init route logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post("/auth/logout", [AuthController::class, 'logout']);
+});
+
 Route::post("/auth/register", [AuthController::class, 'register']);
 Route::post("/auth/login", [AuthController::class, 'login']);
 Route::post("/auth/checkCredentials", [AuthController::class, 'checkCredentias']);
