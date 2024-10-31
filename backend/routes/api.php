8ae9a3d180f535841d5e1af4839995370d571b03
jirayu <jirayu004za@gmail.com>
2024-11-01 01:04:03 +0700

    add route updateUserProfile, getAllProducts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,7 +12,11 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post("/auth/logout", [AuthController::class, 'logout']);
+    // Route::post("/update/profile", [UserController::class, 'updateProfileImage']);
+    Route::post("/update/profile", [UserController::class, 'updateUserProfile']);
 });
+
+Route::get("/products", [ProductController::class, 'getAllProducts']);
 
 Route::post("/auth/register", [AuthController::class, 'register']);
 Route::post("/auth/login", [AuthController::class, 'login']);
