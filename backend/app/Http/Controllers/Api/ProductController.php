bbdcb82e62b9f9f10a7b5663fd8ae464556f8ee3
jirayu <jirayu004za@gmail.com>
2024-11-01 03:55:15 +0700

    change update

--- backend/app/Http/Controllers/Api/ProductController.php
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -10,24 +10,24 @@ class ProductController extends Controller
 {
     public function getAllProducts(Request $request)
     {
-        // $products = Product::all();
-        $products = Product::paginate(10);
+        $products = Product::all();
+        // $products = Product::paginate(10);
 
-        // return response()->json([
-        //     'status' => 'ok',
-        //     'message' => 'Get all products',
-        //     'products' => $products,
-        // ]);
         return response()->json([
             'status' => 'ok',
             'message' => 'Get all products',
-            'products' => [
-                'current_page' => $products->currentPage(),
-                'data' => $products->items(),
-                'last_page' => $products->lastPage(),
-                'total' => $products->total(),
-                'per_page' => $products->perPage(),
-            ],
+            'products' => $products,
         ]);
+        // return response()->json([
+        //     'status' => 'ok',
+        //     'message' => 'Get all products',
+        //     'products' => [
+        //         'current_page' => $products->currentPage(),
+        //         'data' => $products->items(),
+        //         'last_page' => $products->lastPage(),
+        //         'total' => $products->total(),
+        //         'per_page' => $products->perPage(),
+        //     ],
+        // ]);
     }
 }
